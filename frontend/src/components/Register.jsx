2aac29ddc2565b275afdabff849393b5476a67ca
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-02-15 17:09:00 +0530

    working final

---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import { useState, useContext } from "react";
+import { Navigate } from "react-router-dom";
+import axios from "../Axios/axios.js";
+import TokenContext from "../context/TokenContext.js";
+function Register() {
+  const [formData, setFormData] = useState({});
+  const { userToken, tokenDispatch, userDispatch } = useContext(TokenContext);
+  const [error, setError] = useState();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await axios.post("/user/register", formData);
+      tokenDispatch({ type: "SET_TOKEN", payload: result.data.token });
+      userDispatch({ type: "SET_USER", payload: result.data.user });
+      localStorage.setItem("authToken", JSON.stringify(result.data.token));
+    } catch (error) {
+      console.log(error);
+      setError({ message: error.response.data.message });
+    }
+  };
+  return (
+    <div>
+      {userToken && <Navigate to="/" />}
+      <section className="register-container">
+        <div className="container px-6 py-12 h-full">
+          <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+            <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
+              <form method="post" onSubmit={handleSubmit}>
+                <div>
+                  {error && (
+                    <div className="text-center border-2 border-green-600 p-2 mb-2 rounded-md bg-red-200 shadow-2xl">
+                      {error.message}
+                    </div>
+                  )}
+                </div>
+                <div className="mb-6">
+                  <input
+                    type="text"
+                    name="name"
+                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    placeholder="Full name"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-6">
+                  <input
+                    type="text"
+                    name="email"
+                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    placeholder="Email address"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-6">
+                  <input
+                    type="password"
+                    name="password"
+                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="flex justify-between items-center mb-6">
+                  <div className="form-group form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                      id="exampleCheck3"
+                      defaultChecked
+                    />
+                    <label
+                      className="form-check-label inline-block text-gray-800"
+                      htmlFor="exampleCheck2"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+                  data-mdb-ripple="true"
+                  data-mdb-ripple-color="light"
+                >
+                  Register
+                </button>
+                <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+                  <p className="text-center font-semibold mx-4 mb-0">OR</p>
+                </div>
+                <a
+                  className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
+                  style={{ backgroundColor: "#3b5998" }}
+                  href="#!"
+                  role="button"
+                  data-mdb-ripple="true"
+                  data-mdb-ripple-color="light"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 320 512"
+                    className="w-3.5 h-3.5 mr-2"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+                    />
+                  </svg>
+                  Continue with Facebook
+                </a>
+                <a
+                  className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
+                  style={{ backgroundColor: "#55acee" }}
+                  href="#!"
+                  role="button"
+                  data-mdb-ripple="true"
+                  data-mdb-ripple-color="light"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    className="w-3.5 h-3.5 mr-2"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+                    />
+                  </svg>
+                  Continue with LinkedIn
+                </a>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Register;
